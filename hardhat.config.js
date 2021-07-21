53cee48a656b235cb42efe334af523f7b309dfae
VasilijS <catred@mail2000.ru>
2021-07-21 16:51:14 +0700

    add networks to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,39 @@ require('./tasks');
 require('hardhat-docgen');
 require('dotenv').config();
 
+const chainIds = {
+  ganache: 1337,
+  goerli: 5,
+  hardhat: 31337,
+  kovan: 42,
+  mainnet: 1,
+  rinkeby: 4,
+  ropsten: 3,
+};
+
 let mnemonic;
 if (!process.env.MNEMONIC) {
   throw new Error('Please set your MNEMONIC in a .env file');
 } else {
   mnemonic = process.env.MNEMONIC;
+}
+
+let infuraApiKey;
+if (!process.env.INFURA_API_KEY) {
+  throw new Error('Please set your INFURA_API_KEY in a .env file');
+} else {
+  infuraApiKey = process.env.INFURA_API_KEY;
+}
+
+function createNetworkConfig(network) {
+  const url = `https://${network}.infura.io/v3/${infuraApiKey}`;
+  return {
+    accounts: { mnemonic: mnemonic },
+    chainId: chainIds[network],
+    gas: "auto",
+    gasPrice: 50000000000,
+    url: url
+  };
 }
 
 module.exports = {
@@ -17,11 +45,26 @@ module.exports = {
     development: {
       url: "http://127.0.0.1:8545/"
     },
-    testnet: {
+    wqdevnet: {
       url: "https://dev-node-ams3.workquest.co/",
       accounts: { mnemonic: mnemonic },
       gasPrice: 10000000000
-    }
+    },
+    bsctestnet: {
+      url: "https://data-seed-prebsc-2-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: { mnemonic: mnemonic }
+    },
+    bscmainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: { mnemonic: mnemonic }
+    },
+    mainnet: createNetworkConfig('mainnet'),
+    rinkeby: createNetworkConfig('rinkeby'),
+    ropsten: createNetworkConfig('ropsten')
   },
   paths: {
     artifacts: './artifacts',
