63964210237e717dcc1a426f8d431dee481e61cf
VasilijS <catred@mail2000.ru>
2021-07-26 20:53:52 +0700

    fix tasks index

--- tasks/index.js
+++ tasks/index.js
@@ -1,3 +1,6 @@
 require('./transfer_wqt.js');
 require('./update_arbiter.js');
 require('./check_arbiter.js');
+require('./add_token_to_bridge.js');
+require('./set_bridge_to_wqt.js');
+require('./set_bridge_to_wrapped.js');
