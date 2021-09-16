5095d2ddd5c2671b2bf235440bd4ca01244ee475
VasilijS <catred@mail2000.ru>
2021-09-16 12:15:57 +0700

    fix task set_dao_as_admin

--- tasks/index.js
+++ tasks/index.js
@@ -4,3 +4,4 @@ require('./check_arbiter.js');
 require('./add_token_to_bridge.js');
 require('./set_bridge_to_wqt.js');
 require('./set_bridge_to_wrapped.js');
+require('./set_dao_as_admin.js');
--- tasks/set_dao_as_admin.js
+++ tasks/set_dao_as_admin.js
@@ -1,5 +1,5 @@
-tasks("set_DAOvoting_as_admin", "ser DAOvoting as admin for all contracts")
-    .setAction( async function (args, hre) {
+task("set_dao_as_admin", "set DAOVoting contract as admin for all contracts")
+    .setAction(async function (args, hre, runSuper) {
         require('dotenv').config();
         const accounts = await ethers.getSigners();
         const sender = accounts[0].address;
@@ -8,9 +8,7 @@ tasks("set_DAOvoting_as_admin", "ser DAOvoting as admin for all contracts")
         const dotenv = require('dotenv');
         dotenv.config();
         const envConfig = dotenv.parse(fs.readFileSync(`.env-${network}`));
-        for (const k in envConfig) {
-            process.env[k] = envConfig[k]
-        }
+        for (const k in envConfig) { process.env[k] = envConfig[k]; }
         if (!process.env.WORK_QUEST_FACTORY) {
             throw new Error(`Please set your WORK_QUEST_FACTORY in a .env-${network} file`);
         }
@@ -36,14 +34,16 @@ tasks("set_DAOvoting_as_admin", "ser DAOvoting as admin for all contracts")
         const WQPensionFund = await hre.ethers.getContractAt("WQPensionFund", process.env.PENSION_FUND);
         const WQStaking = await hre.ethers.getContractAt("WQStaking", process.env.STAKING);
         const WQToken = await hre.ethers.getContractAt("WQToken", process.env.WORK_QUEST_TOKEN);
-        const WQDAOVoting = await hre.ethers.getContractAt("WQDAOVoting", process.env.DAO_BALLOT)
-        
-        console.log("Start granting ADMIN_ROLE to DAO...")                          
+        const WQDAOVoting = await hre.ethers.getContractAt("WQDAOVoting", process.env.DAO_BALLOT);
+
+        const ADMIN_ROLE = await WQFactory.ADMIN_ROLE();
+
+        console.log("Start granting ADMIN_ROLE to DAO...")
         await WQFactory.grantRole(ADMIN_ROLE, WQDAOVoting.address);
         await WQBridge.grantRole(ADMIN_ROLE, WQDAOVoting.address);
         await WQPensionFund.grantRole(ADMIN_ROLE, WQDAOVoting.address);
         await WQStaking.grantRole(ADMIN_ROLE, WQDAOVoting.address);
         await WQToken.grantRole(ADMIN_ROLE, WQDAOVoting.address);
-        console.log("ADMIN_ROLE was granted to DAO")                          
+        console.log("ADMIN_ROLE was granted to DAO")
 
     });
