7766287326fbd18bf2393f12f3572cbc57f00780
Egor <edergunov@crypton.studio>
2021-09-17 14:49:37 +0700

    add addUserToInsuarance() function

--- contracts/WQInsuranceFactory.sol
+++ contracts/WQInsuranceFactory.sol
@@ -11,7 +11,7 @@ contract WQInsuranceFactory {
     uint256 constant mediumContribution = 2000e18;
     uint256 constant maximalContribution = 3000e18;
 
-    address[] insurances;
+    address[] insurances;   // TO_ASK Is it needed? 
 
     enum PolicyType {
         Minimal,
@@ -24,9 +24,41 @@ contract WQInsuranceFactory {
         Yearly
     }
 
+    struct insuranceInfo {
+        ContributionPeriod period,
+        PolicyType policy, 
+        uint256 usersNum
+    } 
+
+    mapping(ContributionPeriod => mapping(PolicyType => address[])) getLastProperInsuarance;
+    mapping(address => insuranceInfo) insurancesData;
+
     constructor() {}
 
-    function newInsurance(ContributionPeriod _period, PolicyType policyType) external {
+    function addUserToInsuarance(
+        ContributionPeriod _period,
+        PolicyType _policy,
+        address _user
+    ) external {
+        address insuarance = getLastProperInsuarance[_period][_policy];
+        insuranceInfo storage data = insuaranceData[insuarance];
+        if (data.usersNum == 10) {
+            // create new insuarance 
+            address newInsurance =  _newInsurance(_period, _policy);
+            insuaranceInfo storage newData;
+            newData.period = _period;
+            newData.policy = _policy;
+            newData.usersNum = 1;
+            insuarancesData[newInsurance] = newData;
+            getLastProperInsuarance[_period][_policy].push(newInsurance);
+        } else {
+            // add to existance one
+            WQInsurance(insuarance)._addMember(_user);
+            data.usersNum++;
+        }
+    }
+
+    function _newInsurance(ContributionPeriod _period, PolicyType policyType) internal returns (address insurance_){
         uint256 _contributionPeriod;
         if (_period == ContributionPeriod.Monthly) {
             _contributionPeriod = MONTH;
@@ -44,6 +76,7 @@ contract WQInsuranceFactory {
         insurances.push(
             address(new WQInsurance(_contributionPeriod, _contributionAmount))
         );
+        insurance_ = insuarances[insurance.length-1];
     }
 
     function getInsurances() external view returns (address[] memory){
