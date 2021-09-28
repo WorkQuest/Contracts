20069783eaa69367c61da29f6cf45536ee48b3a6
VasilijS <catred@mail2000.ru>
2021-09-28 20:21:54 +0700

    add tasks for swap and redeem

---
+++ tasks/bridge_redeem.js
@@ -0,0 +1,34 @@
+task("bridge_redeem", "Redeem token on bridge")
+    .setAction(async function (args, hre, runSuper) {
+        const accounts = await ethers.getSigners();
+        const validator = accounts[0].address;
+        console.log("Sender address: ", validator);
+
+        const network = hre.network.name;
+        const fs = require('fs');
+        const dotenv = require('dotenv');
+        dotenv.config();
+        const envConfig = dotenv.parse(fs.readFileSync(`.env-${network}`))
+        for (const k in envConfig) { process.env[k] = envConfig[k]; }
+        if (!process.env.BRIDGE) {
+            throw new Error(`Please set your BRIDGE in a .env-${network} file`);
+        }
+        if (!process.env.WQT_TOKEN) {
+            throw new Error('Please set your WORK_QUEST_TOKEN in a .env file');
+        }
+
+        console.log("Redeem on bridge:", process.env.BRIDGE);
+        const bridge = await hre.ethers.getContractAt("WQBridge", process.env.BRIDGE);
+        message = await web3.utils.soliditySha3(
+            { t: 'uint', v: 2 },
+            { t: 'uint', v: "1001000000000000000000" },
+            { t: 'address', v: "0xE24f99419d788003c0D5212f05F47B1572cDC38a" },
+            { t: 'uint256', v: 3 },
+            { t: 'uint256', v: 2 },
+            { t: 'string', v: "WQT" }
+        );
+        let signature = await web3.eth.sign(message, validator);
+        let sig = ethers.utils.splitSignature(signature);
+        await bridge.redeem(2, 3, "1001000000000000000000", "0xE24f99419d788003c0D5212f05F47B1572cDC38a", sig.v, sig.r, sig.s, "WQT");
+        console.log("Done");
+    });
