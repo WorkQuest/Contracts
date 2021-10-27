f662b58758ea10ace13b3b166c7c456cb2be5f93
VasilijS <catred@mail2000.ru>
2021-10-27 12:12:30 +0700

    add upgrading manifests

---
+++ scripts/upgrade_staking_native.js
@@ -0,0 +1,29 @@
+const { ethers, upgrades } = require("hardhat");
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+async function main() {
+  dotenv.config();
+  const accounts = await ethers.getSigners();
+  const sender = accounts[0].address;
+  console.log("Sender address: ", sender);
+
+  const network = hre.network.name;
+  const envConfig = dotenv.parse(fs.readFileSync(`.env-${network}`))
+  for (const k in envConfig) { process.env[k] = envConfig[k]; }
+  if (!process.env.STAKING_NATIVE) {
+    throw new Error(`Please set your STAKING_NATIVE in a .env-${network} file`);
+  }
+
+  console.log("Upgrade...");
+  const WQStakingNative = await ethers.getContractFactory("WQStakingNative");
+  const staking_native = await upgrades.upgradeProxy(process.env.STAKING_NATIVE, WQStakingNative);
+  console.log("WQStakingNative has been upgraded to:", staking_native.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
